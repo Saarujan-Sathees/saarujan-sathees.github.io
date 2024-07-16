c28c3a42426d4ee93545656042e683d4c55290f9
Saarujan Sathees <85376227+Saarujan-Sathees@users.noreply.github.com>
2024-07-15 20:23:43 -0400

    Added Roadmap Animation

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ async function fetchProjects() {
         headers: { "User-Agent": "saarujan-sathees.github.io" }
     })).json();
     const minOffset = -70000, maxOffset = 2000, range = minOffset - 4200, dir = [ "left", "right" ];
-    const skills = document.createElement("pre");
+    const skills = document.createElement("pre"), roadmap = document.getElementById("projectRoadmap");
     skills.classList.add("projectHeader");
     skills.textContent = "Skills";
     projectInfo.range = projectInfo.container.parentElement.clientHeight - projectInfo.container.clientHeight;
@@ -196,6 +196,7 @@ async function fetchProjects() {
     let percentage = 0, distance;
     queueFrame(() => {
         percentage = Math.min(1, Math.max(0, projectInfo.offsetTop / projectInfo.range));
+        roadmap.style.transform = `rotateX(90deg) scaleY(200) translateY(${(1 - percentage) * 230}px)`;
         for (let i = 0; i < projects.length; ++i) {
             distance = offsets[i] + 90000 * percentage;
             projects[i].style.opacity = Math.min(1, 1 - ((distance - 4200) / range));
