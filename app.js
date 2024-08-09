fa8f64c7cc566ee84ea1642d8f34c4705f633e6e
Saarujan Sathees <85376227+Saarujan-Sathees@users.noreply.github.com>
2024-08-09 15:15:05 -0400

    Fixed Resume

--- app.js
+++ app.js
@@ -260,7 +260,8 @@ function loadMedia() {
 
 async function loadResume() {
     const res = await fetch("https://drive.google.com/file/d/14Wo6Dm9EhNlMhBvdfG3fIlODFQ6h4q_k/view");
-    let body = await res.text();
+    let body = await res.text(), start = body.indexOf("src=\"https://drive.google.com/drive-viewer") + 5;
+    body = body.substring(start, body.indexOf("/>", start) - 1);
     console.log(body);
 }
 
